f1a2dc914faf5b6b3bb238101ab3342f3a564f84
Will Bryan Jose <willjose51@gmail.com>
2024-03-15 20:49:57 +0800

    Implement working frontend for smart contract

--- myToken_UI/src/App.svelte
+++ myToken_UI/src/App.svelte
@@ -1,47 +1,89 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { ethers } from "ethers";
+  import type { JsonRpcSigner } from "ethers";
+  import { Contract } from "ethers";
+  import { ABI } from "./abi";
+
+  let stake: number = 0;
+  let rewards: number = 0;
+  let stakeTime: number = 0;
+  const connectWallet = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const account = await provider.send("eth_accounts", []);
+    console.log(account);
+  };
+
+  const getStake = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = await initializeContract(signer);
+    const account = await provider.send("eth_accounts", []);
+    console.log(account);
+    stake = await contract.getStake(account[0]);
+    console.log(stake);
+  };
+
+  const stakeTokens = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = await initializeContract(signer);
+    await contract.stake(100);
+    alert("100 tokens staked");
+    getStake();
+    getLastStake();
+    getRewards();
+  };
+
+  const withdrawStakedTokens = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = await initializeContract(signer);
+    await contract.withdraw();
+    alert("Staked tokens withdrawn");
+    getStake();
+    getLastStake();
+    getRewards();
+  };
+
+  const getRewards = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = await initializeContract(signer);
+    const account = await provider.send("eth_accounts", []);
+    rewards = await contract.getCurrentRewards(account[0]);
+  };
+
+  const getLastStake = async () => {
+    const { ethereum } = window as any;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    const contract = await initializeContract(signer);
+    const account = await provider.send("eth_accounts", []);
+    stakeTime = await contract.getLastStakeTimestamp(account[0]);
+  };
+
+  const initializeContract = async (signer: JsonRpcSigner) => {
+    return new Contract(
+      "0x6C080Cf2e785F7A186Ce688f1d20e5D210e09AA6",
+      ABI,
+      signer
+    );
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <button on:click={connectWallet}>Connect Wallet</button>
+  <br>
+  <p>Stake: {stake}</p>
+  <p>Update rewards: {rewards}</p>
+  <p>Staked Time: {stakeTime}</p>
+  <br>
+  <button on:click={stakeTokens}>Stake 100</button>
+  <button on:click={withdrawStakedTokens}>Withdraw</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
